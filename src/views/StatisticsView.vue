0ad1117aab3f914dfb8dfd1202add5102e78d8b0
KapitanPiratu <janek.marek@outlook.cz>
2023-07-18 19:02:28 +0200

    views setup

---
+++ src/views/StatisticsView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>statistics</h1>
+</template>
---
+++ src/views/TimerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TimerClock />
+  </div>
+</template>
+
+<script>
+import TimerClock from '@/components/timer/TimerClock.vue'
+
+export default {
+  name: 'TimerView',
+  components: {
+    TimerClock
+  }
+}
+</script>
