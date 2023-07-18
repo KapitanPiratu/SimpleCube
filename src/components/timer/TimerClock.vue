4151ab04002cdcc5e09198c8124d0d9e4c9d97b8
KapitanPiratu <janek.marek@outlook.cz>
2023-07-18 19:29:50 +0200

    TimerClock setup + some basic styles

--- src/components/timer/TimerClock.vue
+++ src/components/timer/TimerClock.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Timer</h1>
+  <p id="current-time-display">{{currentTime}}</p>
 </template>
 
 <script>
-export default {
-  name: 'TimerClock'
-}
+  export default {
+    name: 'TimerClock',
+    data() {
+      return {
+        currentTime: 0
+      }
+    },
+  }
 </script>
+
+<style>
+  #current-time-display{
+    font-size: 7rem;
+    position: fixed;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
