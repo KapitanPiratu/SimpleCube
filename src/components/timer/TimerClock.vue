383311c6a97b845b57e2a3aa5e6926e62c4e7adc
KapitanPiratu <janek.marek@outlook.cz>
2023-09-10 11:31:44 +0200

    added inspection countdown

--- src/components/timer/TimerClock.vue
+++ src/components/timer/TimerClock.vue
@@ -11,6 +11,7 @@
         timeStart: null,
         timeEnd: null,
         timerStatus: 'neutral',
+        inspectionRunning: false,
         step: 0,
         Steps: [
           {
@@ -45,24 +46,50 @@
       readyToInspect(){
         this.timerStatus = 'ready';
       },
-      startInspection(){
+      wait(ms){
+        return new Promise(resolve => this.timeoutid = setTimeout(resolve, ms));
+      },
+      async startInspection(){
         this.timerStatus = 'inspection';
-        this.currentTime = 'inspection';
+        this.currentTime = 15;
+        this.inspectionRunning = true
+        let i = 0;
+        while (i < 17 && this.inspectionRunning){
+          await this.wait(1000)
+          .then(() => {
+            if (i <= 13) this.currentTime--;
+          });
+          if (i > 5) {
+            this.timerStatus = 'orange';
+          }
+          if (i > 13){
+            this.currentTime = '+2';
+          }
+          if (i > 15) {
+            this.currentTime = 'DNF';
+            this.timerStatus = 'red'
+          }
+          i++;
+        }
       },
       readyToSolve(){
         this.timerStatus = 'ready';
       },
       startTimer(){
         this.timeStart = Date.now();
-        this.timerStatus = 'solving'
-        this.currentTime = 'solving'
+        this.timerStatus = 'solving';
+        this.currentTime = 'solving';
+        this.inspectionRunning = false;
+        clearTimeout(this.timeoutid);
       },
       stopTimer(){
         this.timeEnd = Date.now();
         this.currentTime = (this.timeEnd - this.timeStart) / 1000;
         this.timerStatus = 'neutral'
       },
-      handleStep(type){
+      handleStep(type, e){
+        if (e.code != 'Space') return; //allow only space for now
+
         if (this.Steps[this.step].nextEventType == type){
           this.Steps[this.step].func();
           if (this.step < 4){
@@ -74,8 +101,8 @@
       }
     },
     mounted() {
-      window.addEventListener('keydown', () => this.handleStep('down'));
-      window.addEventListener('keyup', () => this.handleStep('up'));
+      window.addEventListener('keydown', (e) => this.handleStep('down', e));
+      window.addEventListener('keyup', (e) => this.handleStep('up', e));
     },
   }
 </script>
@@ -95,5 +122,13 @@
 
   .inspection{
     color: white
+  }
+
+  .orange{
+    color: orange;
+  }
+
+  .red{
+    color: red;
   }
 </style>
